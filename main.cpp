9704f285417f373301fbde2c7acb918b9fdba2f9
Rugal <rugal.bernstein.0@gmail.com>
2016-08-21 19:14:55 -0400

    Init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Rugal Bernstein
+ *
+ * Created on August 21, 2016, 7:06 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
